512ff3907162f1e676f597f16d04c32584a573fb
nozloy <31240637+nozloy@users.noreply.github.com>
2022-04-23 21:04:58 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <h2>{{test}} {{theme}} {{user}}</h2>
+</template>
+
+<script>
+export default {
+    setup()
+    {
+        //let theme = window.Telegram.WebApp.colorScheme;
+        //let user = window.Telegram.WebApp.initParams;
+        let test = "тест"
+        let user = "user"
+        let theme = "theme"
+        return {test, user, theme}
+    },
+    mounted() {
+        let telegramScript = document.createElement('script')
+      telegramScript.setAttribute('src', 'https://telegram.org/js/telegram-web-app.js')
+      document.head.appendChild(telegramScript)
+    }
+}
+</script>
+
+<style>
+
+</style>
