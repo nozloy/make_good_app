656d67b57deb3d4ff45e04ff91dffa40f3084912
nozloy <nozloy@gmail.com>
2022-04-25 00:39:10 +0000

    from server

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-    <h2>{{test}} {{theme}} {{user}}</h2>
+  <v-app>
+    <v-main>
+      <user-info/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+import UserInfo from './Components/UserInfo.vue'
+
 export default {
-    setup()
-    {
-        let theme = window.Telegram.WebApp.colorScheme;
-        let user = window.Telegram.WebApp.initData;
-        let test = "тест"
-        //let user = "юзер"
-        //let theme = "тема"
-        return {test, user, theme}
-    },
-    mounted() {
-        let telegramScript = document.createElement('script')
-      telegramScript.setAttribute('src', 'https://telegram.org/js/telegram-web-app.js')
-      document.head.appendChild(telegramScript)
-    }
+  name: 'App',
+
+  components: {
+    UserInfo,
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-
-</style>
---
+++ src/Components/UserInfo.vue
@@ -0,0 +1,55 @@
+<template>
+<v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="align-end text-white"
+      height="200"
+      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      cover
+    >
+      <v-card-title>Информация о пользователе</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pt-4">
+      {{userFirstName}} {{userLastName}} {{username}}
+    </v-card-subtitle>
+
+    <v-card-text>
+      <div>Активная теама: {{theme}}</div>
+
+      <div>Query ID: {{queryId}}</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="orange">
+        Подробнее
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+    setup()
+    {
+        let theme = window.Telegram.WebApp.colorScheme;
+        const initData = window.Telegram.WebApp.initData;
+        const infoArray = initData.split("&");
+        const userData = JSON.parse((decodeURIComponent(infoArray[1])).slice(5));
+        const userFirstName = userData.first_name;
+        const userLastName = userData.last_name;
+        const id = userData.id;
+        const username = userData.username;
+        const queryId = (infoArray[0]).slice(9);
+        const authDate = (infoArray[2]).slice(10);
+        const hash = (infoArray[3]).slice(5);
+        return {userFirstName, userLastName, id, username, queryId, authDate , hash, theme}
+    }
+}
+</script>
+
+<style>
+
+</style>
