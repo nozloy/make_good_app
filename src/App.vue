88ef259510dfb29596dff2db8ea64d64deec5191
nozloy <31240637+nozloy@users.noreply.github.com>
2022-04-26 18:14:46 +0300

    next stage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <v-main>
+      
       <menu-table-food/>
     </v-main>
   </v-app>
---
+++ src/components/StandartTable.vue
@@ -0,0 +1,80 @@
+<template>
+<div class="standarttable">
+    <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">
+          Холодные закуски
+        </th>
+        <th class="text-left">
+          Порция
+        </th>
+        <th class="text-left">
+          Количество
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in snacks"
+        :key="item.name"
+      >
+        <td>{{ item.name }}</td>
+        <td>{{ item.weight }}</td>
+          <td>
+            {{ item.quantity }}
+            <v-btn
+        variant="text"  
+          @click="removeItem(item)"  
+          size="small"
+          icon="mdi-minus-circle"
+          color="error"
+           ></v-btn>  
+            <v-btn
+        variant="text"  
+          @click="addItem(item)"  
+          size="small"
+          icon="mdi-plus-circle"
+          color="success"
+           ></v-btn>
+          </td>
+      </tr>
+    </tbody>
+  </v-table>
+</div> 
+</template>
+
+<script>
+export default {
+    props: {
+        snacks: {
+            type:Array,
+            required: true,
+        }
+    },
+    data() {
+        return{
+        }
+    },
+        methods:{
+      addItem (item){
+        if(item.quantity < 5){
+        item.quantity +=1;
+        }
+      },
+      removeItem (item){
+        if(item.quantity > 0){
+        item.quantity -=1;
+        } else {
+          item.quantity = 0;
+        }
+      }
+    }
+    
+
+}
+</script>
+
+<style>
+
+</style>
