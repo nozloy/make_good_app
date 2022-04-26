3fd0f8371a5840e0864fd425f207b27169a87f95
nozloy <31240637+nozloy@users.noreply.github.com>
2022-04-26 18:14:33 +0300

    next stage

--- src/components/MenuTableFood.vue
+++ src/components/MenuTableFood.vue
@@ -1,44 +1,13 @@
 <template>
-   <v-table>
-    <thead>
-      <tr>
-        <th class="text-left">
-          Холодные закуски
-        </th>
-        <th class="text-left">
-          Порция
-        </th>
-        <th class="text-left">
-          Количество
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="item in snacks"
-        :key="item.name"
-      >
-        <td>{{ item.name }}</td>
-        <td>{{ item.weight }}</td>
-          <td><v-btn @click="removeItem(item)"
-          size="x-small"
-          icon="mdi-chevron-left-circle"
-          color="success"
-           ></v-btn>
-        {{item.quantity}}
-            <v-btn @click="addItem(item)"
-          size="x-small"
-          icon="mdi-chevron-right-circle"
-          color="success"
-            ></v-btn>
-          </td>
-      </tr>
-    </tbody>
-  </v-table> 
+ <standart-table :snacks="snacks"/>
 </template>
 
 <script>
+import StandartTable from './StandartTable.vue';
   export default {
+      components: {
+    StandartTable,
+  },
     data () {
       return {
         snacks: [
@@ -95,21 +64,7 @@
         ],
       }
     },
-    methods:{
-      addItem (item){
-        item.quantity += 1;
-        console.log(item.quantity);
-      },
-      removeItem (item){
-        if(item.quantity > 0){
-        item.quantity -=1;
-        } else {
-          item.quantity = 0;
-        }
-        
-        console.log(item.quantity)
-      }
-    }
+
   }
 </script>
 
